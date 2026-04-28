0d7883c3074f484637b631b73737ae60606df67a
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-04-28 23:40:24 +0200

    Ex4 firstAvailableRow

--- src/main/scala/ex4/ConnectThree.scala
+++ src/main/scala/ex4/ConnectThree.scala
@@ -10,7 +10,6 @@ object ConnectThree extends App:
       case _ => X
 
   case class Disk(x: Int, y: Int, player: Player)
-  //
   // Board:
   // y
   //
@@ -18,8 +17,7 @@ object ConnectThree extends App:
   // 2
   // 1
   // 0
-  //   0 1 2 3 <-- x
-  //
+  //   0 1 2 3   x
   type Board = Seq[Disk]
   type Game = Seq[Board]
 
@@ -28,7 +26,13 @@ object ConnectThree extends App:
   def find(board: Board, x: Int, y: Int): Option[Player] =
     board.find(disk => disk.x == x && disk.y == y).map(_.player)
 
-  def firstAvailableRow(board: Board, x: Int): Option[Int] = ???
+  def firstAvailableRow(board: Board, x: Int): Option[Int] =
+    board
+      .collect { case Disk(`x`, y, _) => y }
+      .maxOption
+      .map(_ + 1)
+      .orElse(Some(0))
+      .filter(_ <= bound)
 
   def placeAnyDisk(board: Board, player: Player): Seq[Board] = ???
 
@@ -52,8 +56,8 @@ object ConnectThree extends App:
   println(find(List(Disk(0, 0, X), Disk(0, 1, O), Disk(0, 2, X)), 0, 1))
   println(find(List(Disk(0, 0, X), Disk(0, 1, O), Disk(0, 2, X)), 1, 1)) // None
 
-  // Exercise 2: implement firstAvailableRow such that...
-  println("EX 2: ")
+  // Exercise 3: implement firstAvailableRow such that...
+  println("EX 3: ")
   println(firstAvailableRow(List(), 0)) // Some(0)
   println(firstAvailableRow(List(Disk(0, 0, X)), 0)) // Some(1)
   println(firstAvailableRow(List(Disk(0, 0, X), Disk(0, 1, X)), 0)) // Some(2)
@@ -66,7 +70,9 @@ object ConnectThree extends App:
       0
     )
   ) // None
+
   // Exercise 2: implement placeAnyDisk such that...
+  println("EX 2: ")
   printBoards(placeAnyDisk(List(), X))
   // .... .... .... ....
   // .... .... .... ....
@@ -77,22 +83,22 @@ object ConnectThree extends App:
   // .... .... .... ....
   // ...X .... .... ....
   // ...O ..XO .X.O X..O
-  println("EX 4: ")
 
-// Exercise 3 (ADVANCED!): implement computeAnyGame such that...
+  // Exercise 3 (ADVANCED!): implement computeAnyGame such that...
   computeAnyGame(O, 4).foreach { g =>
     printBoards(g)
     println()
   }
-//  .... .... .... .... ...O
-//  .... .... .... ...X ...X
-//  .... .... ...O ...O ...O
-//  .... ...X ...X ...X ...X
-//
-//
-// .... .... .... .... O...
-// .... .... .... X... X...
-// .... .... O... O... O...
-// .... X... X... X... X...
 
-// Exercise 4 (VERY ADVANCED!) -- modify the above one to stop each game when someone won!!
+  //  .... .... .... .... ...O
+  //  .... .... .... ...X ...X
+  //  .... .... ...O ...O ...O
+  //  .... ...X ...X ...X ...X
+  //
+  //
+  // .... .... .... .... O...
+  // .... .... .... X... X...
+  // .... .... O... O... O...
+  // .... X... X... X... X...
+
+  // Exercise 4 (VERY ADVANCED!) -- modify the above one to stop each game when someone won!!
